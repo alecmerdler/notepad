43765dc33580487904895b7564915ff69efed0ba
alecmerdler <merdlera@oregonstate.edu>
2017-03-11 00:47:14 -0800

    simple stop-and-go langauge in Haskell/TypeScript

--- functional-typescript.ts
+++ functional-typescript.ts
@@ -1,13 +1,51 @@
-var add: (a: number) => (b: number) => number;
-add = (a: number) => (b: number) => a + b;
+//
+// Attempting to write TypeScript in as close to Haskell syntax as possible.
+//
+
+type Go = {kind: "Go"}
+type Stop = {kind: "Stop"};
+
+type Cmd = Go
+         | Stop;
+
+type State = boolean;
+type Domain = boolean | null;
 
 
+//
+// | Denotational semantics for a simple stop-and-go language.
+//   The commands are either Stop or Go.
+//   The semantic domain is whether we are stopped (true if last command was Go, false if last command was Stop).
+//     Returns null if we attempted to perform the same action that we did before.
+//
+//   cmd :: Cmd -> State -> Domain
+//
+var cmd: (a: Cmd) => (state: State) => Domain;
+cmd = (a: Cmd) => {
+    switch (a.kind) {
+        case "Go": return (state: State) => {
+            if (!state) return true;
+            else return null;
+        };
+        case "Stop": return (state: State) => {
+            if (state) return false;
+            else return null;
+        };
+    }
+};
 
 
+//
+// Testing
+//
+var domain: Domain = cmd({kind: "Go"})(false);
+console.assert(domain == true);
 
+domain = cmd({kind: "Stop"})(true);
+console.assert(domain == false);
 
-/**
- * Testing
- */
-console.assert(add(1)(1) == 2);
+domain = cmd({kind: "Go"})(true);
+console.assert(domain == null);
 
+domain = cmd({kind: "Stop"})(false);
+console.assert(domain == null);
